1daaedcb280743358601ce9237429bfdde19fc7b
Camilo Tobon <1611@holbertonschool.com>
2020-07-27 21:24:22 -0500

    adding timer for performance

--- src/python_scripts/main.py
+++ src/python_scripts/main.py
@@ -1,7 +1,54 @@
 import re
 import string
 import random
-import libmyrustlib   #  <-- Import the Rust implemented module (myrustlib.so)
+import libmyrustlib
+import numpy as np
+from timeit import default_timer as timer
+
+# TESTING RUST VS PYTHON PERFORMANCE
+
+def bubbleSort(arr): 
+    n = len(arr) 
+    for i in range(n): 
+        swapped = False
+        for j in range(0, n-i-1): 
+
+            if arr[j] > arr[j+1] : 
+                arr[j], arr[j+1] = arr[j+1], arr[j] 
+                swapped = True
+        if swapped == False: 
+            break
+
+arr = list(np.random.randint(0,20000, 20000))
+
+def test_bubble_sort1():
+    libmyrustlib.sort_list(arr)
+
+def test_bubble_sort2():
+    bubbleSort(arr)
+
+print(f"List with -> {len(arr)} elements")
+
+start = timer()
+test_bubble_sort1()
+end = timer()
+print(f"Bubble sort from Rust takes {end - start}") 
+
+start = timer()
+test_bubble_sort2()
+end = timer()
+print(f"Bubble sort from Python takes {end - start}") 
+
+
+
+
+
+
+
+
+
+# implementation for counting doubles (with rust)
+""" 
 
 
 def get_dict_value():
@@ -10,15 +57,9 @@ def get_dict_value():
     valueFromRust = libmyrustlib.get_value_from_rust(data, tofind)
     print(f"Rust returned {valueFromRust} from {tofind}")
 
-def sort_list():
-    x = libmyrustlib.sort_list([50, 30, 45, 23, 43, 1])
-    print(x)
-
-sort_list()
 
 
-# implementation for counting doubles (with rust)
-""" def count_doubles(val):
+def count_doubles(val):
     total = 0
     for c1, c2 in zip(val, val[1:]):
         if c1 == c2:
